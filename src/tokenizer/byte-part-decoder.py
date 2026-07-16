fc5167a483f8ca0daa0114bba3f0aa014349d5eb
Malachi Chandler <mc10398@nyu.edu>
2026-07-15 22:50:28 -0400

    Delete src/tokenizer/byte-part-decoder.py
    removed redundant file

--- src/tokenizer/byte-part-decoder.py
+++
@@ -1,7 +0,0 @@
-# BPE O(n) decoder
-def BPDecoder(encoded_text, decodeMap):
-  decoded = ""
-  for word in range(len(encoded_text)):
-    for char in range(len(encoded_text[word])):
-      decoded += decodeMap[encoded_text[word][char]]
-  return decoded
